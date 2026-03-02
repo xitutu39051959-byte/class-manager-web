bcf61b5c13c928263fd61dff4a0e5763f0bc598c
xitutu39051959-byte <xitutu39051959@gmail.com>
2026-03-02 08:28:20 +0800

    Set base path for deployment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/class-manager-web/',   // ← 必须和仓库名完全一致
   plugins: [react()],
 })
